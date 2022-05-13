c9af8610c9aab5b5f4bb48aab75cfad5ff835a23
Bob D <txcavscout@live.com>
2022-05-12 22:52:52 -0700

    Add project files.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,27 @@
+﻿List<string> firstName = new ();
+string? insert;
+
+do
+{
+    Console.Write("Enter a first name. Type end to quit: ");
+    insert = Console.ReadLine();
+
+    //checks for no name entry
+    if (string.IsNullOrEmpty(insert))
+    {
+        Console.WriteLine("You must enter a name!");
+    }
+    else if (insert != "end")
+    {
+        
+        firstName.Add(insert);
+    }
+
+} while (insert != "end");
+
+Console.WriteLine("\n");
+
+foreach (var name in firstName)
+{
+    Console.WriteLine($"Hello {name}");
+}
